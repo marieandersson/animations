390b981e3afd1ab84355bfc94cf457b5dff3f560
Marie Eriksson <eriksson.km@gmail.com>
2017-10-31 22:04:34 +0100

    loop bubble animation

--- src/components/animations/BubbleOne.vue
+++ src/components/animations/BubbleOne.vue
@@ -8,9 +8,8 @@
           <stop offset="1" stop-color="#f40000"/>
         </linearGradient>
       </defs>
-      <title>animation1-start</title>
-      <path class="cls-1 grey" d="M257.18,131.28c0,50.09-38.6,114.79-99.24,114.79S41.82,197.43,41.82,147.35,86.75,28,147.39,28,257.18,81.19,257.18,131.28Z"/>
-      <path class="cls-2 pink" d="M244.76,145.93c0,34.63-24.72,79.38-63.55,79.38S106.84,191.67,106.84,157s28.78-82.55,67.61-82.55S244.76,111.29,244.76,145.93Z"/>
+      <path class="cls-1 grey" :d="this.greyAnimation.paths[0]" />
+      <path class="cls-2 pink" :d="this.pinkAnimation.paths[0]" />
     </svg>
 
   </div>
@@ -21,27 +20,49 @@ const Snap = require( "imports-loader?this=>window,fix=>module.exports=0!snapsvg
 
 export default {
   name: 'bubbleOne',
-  // data : {
-  //
-  //     // set paths as data?
-  //
-  // },
+  data () {
+    return {
+      // SVG paths
+      greyAnimation: {
+        paths: [
+          'M257.18,131.28c0,50.09-38.6,114.79-99.24,114.79S41.82,197.43,41.82,147.35,86.75,28,147.39,28,257.18,81.19,257.18,131.28Z',
+          'M258.09,132.07c-1.6,59-54.94,111-96.34,111-64.34,0-98.2-70.22-98.2-104.43,0-96,37.71-144,99.55-134C222.37,14.25,260,60.67,258.09,132.07Z'
+        ]
+      },
+      pinkAnimation: {
+        paths: [
+          'M244.76,145.93c0,34.63-24.72,79.38-63.55,79.38S106.84,191.67,106.84,157s28.78-82.55,67.61-82.55S244.76,111.29,244.76,145.93Z',
+          'M174.89,165.28c0,23.65,58,111.07-11.37,111.07-73.79,0-44.8-82.05-44.8-105.7S61.76,70,153.55,64.93C261,59,174.89,141.62,174.89,165.28Z'
+        ]
+      },
+      animating: false
+    }
+  },
   methods: {
     animate () {
-      const bubble = this.$el.querySelector('.bubble-one-svg');
-      const s = Snap(bubble);
-      const grey = Snap.select('.grey');
-      const pink = Snap.select('.pink');
-
-      const greyPathOne = 'M257.18,131.28c0,50.09-38.6,114.79-99.24,114.79S41.82,197.43,41.82,147.35,86.75,28,147.39,28,257.18,81.19,257.18,131.28Z';
-      const pinkPathOne = 'M244.76,145.93c0,34.63-24.72,79.38-63.55,79.38S106.84,191.67,106.84,157s28.78-82.55,67.61-82.55S244.76,111.29,244.76,145.93Z';
-      const greyPathTwo = 'M258.09,132.07c-1.6,59-54.94,111-96.34,111-64.34,0-98.2-70.22-98.2-104.43,0-96,37.71-144,99.55-134C222.37,14.25,260,60.67,258.09,132.07Z';
-      const pinkPathTwo = 'M174.89,165.28c0,23.65,58,111.07-11.37,111.07-73.79,0-44.8-82.05-44.8-105.7S61.76,70,153.55,64.93C261,59,174.89,141.62,174.89,165.28Z';
-      grey.animate({ d: greyPathTwo}, 1000, mina.linear);
-      pink.animate({d: pinkPathTwo}, 1000, mina.easeinout);
+      if (this.animating) {
+        return;
+      }
+      this.animating = true;
+      let self = this;
+      self.runAnimation(1, function () {
+        self.runAnimation(0, function () {
+          console.log('done');
+          requestAnimationFrame(self.animate);
+          self.animating = false;
+        })
+      });
+    },
+    runAnimation (index, callback) {
+      this.grey.animate({ d: this.greyAnimation.paths[index] }, 1000, mina.linear);
+      this.pink.animate({ d: this.pinkAnimation.paths[index] }, 1000, mina.easeinout, callback);
     }
   },
   mounted () {
+    const bubble = this.$el.querySelector('.bubble-one-svg');
+    const s = Snap(bubble);
+    this.grey = Snap.select('.grey');
+    this.pink = Snap.select('.pink');
   }
 }
 </script>
@@ -58,5 +79,4 @@ export default {
     }
   }
 }
-
 </style>
