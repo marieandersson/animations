6944942c981ee2696e96228cc8b59f94b484065d
Marie Eriksson <eriksson.km@gmail.com>
2017-11-05 16:15:56 +0100

    add heading canvas

---
+++ src/components/animations/HeadingText.vue
@@ -0,0 +1,73 @@
+<template>
+    <canvas class="heading-canvas"></canvas>
+</template>
+
+<script>
+
+export default {
+  name: 'headingText',
+  data () {
+    return {
+      activeState: 'start',
+      iterations: 100,
+      textPosition: {
+        x: 0,
+        y: 450
+      },
+      textVelocity: {
+        x: 6,
+        y: -3
+      }
+
+    }
+  },
+  methods: {
+    drawText(context) {
+      context.font = 'bold 80px Poppins';
+      context.textAlign = 'right';
+      context.lineWidth = '1';
+      context.textBaseline = 'middle';
+
+      const startRgb = [244, 0, 0];
+      const stopRgb = [255, 0, 152];
+      let currentRgb = [startRgb[0], startRgb[1], startRgb[2]];
+
+      for (let i = 0; i < this.iterations; i++) {
+        currentRgb[0] = currentRgb[0] + ((stopRgb[0] - startRgb[0]) / this.iterations);
+        currentRgb[1] = currentRgb[1] + ((stopRgb[1] - startRgb[1]) / this.iterations);
+        currentRgb[2] = currentRgb[2] + ((stopRgb[2] - startRgb[2]) / this.iterations);
+        this.textPosition.x = this.textPosition.x + this.textVelocity.x;
+        this.textPosition.y = this.textPosition.y + this.textVelocity.y;
+        context.fillStyle = `rgb(${Math.round(currentRgb[0])}, ${Math.round(currentRgb[1])}, ${Math.round(currentRgb[2])})`;
+        context.fillText('ANIMATIONS', this.textPosition.x, this.textPosition.y);
+        context.strokeStyle = '#FFF';
+        context.strokeText('ANIMATIONS', this.textPosition.x, this.textPosition.y);
+      }
+    }
+  },
+  mounted() {
+    const canvas = this.$el;
+    const context = canvas.getContext('2d');
+    // scale canvas to look good on retina
+    const scale = window.devicePixelRatio || 1;
+    canvas.height = window.innerHeight * scale / 1.5;
+    canvas.width = window.innerWidth * scale / 2;
+    context.scale(scale, scale);
+
+    if (window.devicePixelRatio > 1) {
+      canvas.style.height = `${canvas.height / 2}px`;
+      canvas.style.width = `${canvas.width / 2}px`;
+    }
+    // center positions for all screens
+    const centerWidth = canvas.width / (2 * scale);
+    const centerHeight = canvas.height / (2 * scale);
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.drawText(context);
+  }
+}
+
+</script>
+
+<style lang="scss">
+</style>
--- src/components/partials/Heading.vue
+++ src/components/partials/Heading.vue
@@ -1,65 +1,26 @@
 <template>
-  <div class="header">
-  <!-- animate heading with canvas ? -->
-    <h1>Rubrik</h1>
-    <h2>Type somehing</h2>
-  </div>
+  <header>
+   <headingText></headingText>
+  </header>
 </template>
 
 <script>
+import headingText from '../animations/HeadingText.vue';
 
 export default {
-  name: 'header',
+  name: 'heading',
+  components: {
+    headingText
+  }
 }
 </script>
 
 <style lang="scss">
-.header {
+header {
   width: 100%;
   height: 100vh;
   border-bottom: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-  padding: 15% 0 0 15%;
   margin: 0;
-  h1 {
-    transition: all .3s linear;
-    font-size: 8em;
-    color: #fff;
-    margin: 0;
-  }
-  h2 {
-    transition: all .3s linear;
-  }
 }
-.start .header {
-  h1 {
-    text-shadow: -5px 5px 0 #000;
-  }
-}
-.scroll .header {
-  h1 {
-    text-shadow: -5px 5px 0 #FF0098;
-  }
-  h2 {
-    color: #FF0098;
-  }
-}
-.hover .header {
-  h1 {
-    text-shadow: -5px 5px 0 #EE3F00;
-  }
-  h2 {
-    color: #EE3F00;
-  }
-}
-.click .header {
-  h1 {
-    text-shadow: -5px 5px 0 #1808EF;
-  }
-  h2 {
-    color: #1808EF;
-  }
-}
+
 </style>
