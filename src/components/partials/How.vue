1d4410e2514b2a239118e3f63a0ff8d6d3bd9791
Marie Eriksson <eriksson.km@gmail.com>
2017-11-06 21:53:47 +0100

    position ux section

--- src/components/partials/How.vue
+++ src/components/partials/How.vue
@@ -34,7 +34,6 @@ export default {
       this.activeState = clickedNavItem;
     });
   }
-
 }
 </script>
 
--- src/components/partials/Ux.vue
+++ src/components/partials/Ux.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="ux">
     <div class="ux-text">
-      <h2>What not to</h2>
+      <h2>What not to!</h2>
+      <p> {{ texts[activeState] }} </p>
+    </div>
+    <div class="ux-text">
+      <h2>What not to!</h2>
       <p> {{ texts[activeState] }} </p>
     </div>
   </div>
@@ -16,7 +20,7 @@ export default {
       activeState: 'start',
       texts: {
         start: 'Start: Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden.',
-        scroll: 'Scroll: Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden.',
+        scroll: 'Scroll: Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
         hover: 'Hover: Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden.',
         click: 'Click: Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden.'
       }
@@ -33,20 +37,44 @@ export default {
 
 <style lang="scss">
 .ux {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 0 10%;
+  width: calc(100% - 300px);
+  background: #fff;
+  height: 700px;
+  position: relative;
+  margin: 150px;
+  border: solid 10px;
   .ux-text {
-    width: 500px;
+    position: absolute;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: row;
     margin: 0;
-    width: 100%;
     justify-content: center;
+    align-items: center;
+    padding-right: 8em;
+    h2 {
+      transform: rotate(-90deg) translate(105px, -30px);
+    }
+    p {
+      width: 500px;
+    }
+    &:hover {
+      transform: translate(2px, 2px);
+    }
   }
+}
+.start .ux {
+  border-color: #000;
+}
+.scroll .ux {
+  border-color: #F40000;
+}
+.hover .ux {
+  border-color: #EE3F00;
+}
+.click .ux {
+  border-color: #001F44;
 }
 
 </style>
